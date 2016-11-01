cb77d58cd1c8039f0af48a964f6ca2be6017f829
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-10-31 19:25:24 -0600

    chore(init) Init commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var debug = process.env.NODE_ENV !== "production";
+var path = require("path");
+var webpack = require('webpack');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+  entry: SRC_DIR + "/app/index.js",
+  output: {
+    path: DIST_DIR + "/app",
+    filename: "bundle.js",
+    publicPath: "/app/"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015"]
+        }
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
+
+module.exports = config;
