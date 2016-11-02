22a5a39e6adfeb0c9e3e4d4d0145278b35b6c154
Matt Sadauckas <matthewsadauckas@gmail.com>
2016-11-02 17:07:34 -0600

    chore(config) Debug Heroku hosting

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,17 @@ var config = {
         exclude: /(node_modules|bower_components)/
       }
     ]
-  }
+  },
+  plugins: debug ? [] : [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(true),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({})
+]
 };
 
 module.exports = config;
